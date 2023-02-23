28b8657f43efc74db548b0da56bb546af8ad4aa5
TheDevick <erick@bilhalba.com.br>
2023-02-23 08:19:37 +0000

    test: create Client Test

---
+++ tests/ClientTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use NystronSolar\GrowattSpreadsheet\Client\Client;
+use PHPUnit\Framework\TestCase;
+
+class ClientTest extends TestCase
+{
+    public function testClientGetterSetter()
+    {
+        $fakePlantName = 'FakeHouse';
+        $fakeUserAccountName = 'FakeUser';
+        $fakeCity = 'City';
+        $fakeDeviceCount = 1;
+        $fakeCreateDate = new DateTime();
+        $fakeTotalComponentPower = 2500.0;
+
+        $fakeClient = (new Client())
+            ->setPlantName($fakePlantName)
+            ->setUserAccountName($fakeUserAccountName)
+            ->setCity($fakeCity)
+            ->setDeviceCount($fakeDeviceCount)
+            ->setCreateDate($fakeCreateDate)
+            ->setTotalComponentPower($fakeTotalComponentPower)
+        ;
+
+        $this->assertSame($fakePlantName, $fakeClient->getPlantName());
+        $this->assertSame($fakeUserAccountName, $fakeClient->getUserAccountName());
+        $this->assertSame($fakeCity, $fakeClient->getCity());
+        $this->assertSame($fakeDeviceCount, $fakeClient->getDeviceCount());
+        $this->assertSame($fakeCreateDate, $fakeClient->getCreateDate());
+        $this->assertSame($fakeTotalComponentPower, $fakeClient->getTotalComponentPower());
+    }
+}
