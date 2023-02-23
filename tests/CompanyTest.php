654ac6ef9acb1cb7a57b1e74e0fa1a4d99936114
TheDevick <erick@bilhalba.com.br>
2023-02-23 06:29:46 +0000

    test: create Company Test

---
+++ tests/CompanyTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use NystronSolar\GrowattSpreadsheet\Company;
+use PHPUnit\Framework\TestCase;
+
+class CompanyTest extends TestCase
+{
+    public function testGetterSetter()
+    {
+        $fakeCompanyName = "Fake Company";
+        $fakeCompanyCode = "FAKE1";
+        $fakeCompanyTotalComponentPower = 10000;
+        $fakeCompanyEnergyTotal = 5000.0;
+
+        $fakeCompany = new Company();
+        $fakeCompany
+            ->setName($fakeCompanyName)
+            ->setCode($fakeCompanyCode)
+            ->setTotalComponentPower($fakeCompanyTotalComponentPower)
+            ->setEnergyTotal($fakeCompanyEnergyTotal)
+        ;
+
+        $this->assertSame($fakeCompanyName, $fakeCompany->getName());
+        $this->assertSame($fakeCompanyCode, $fakeCompany->getCode());
+        $this->assertSame($fakeCompanyTotalComponentPower, $fakeCompany->getTotalComponentPower());
+        $this->assertSame($fakeCompanyEnergyTotal, $fakeCompany->getEnergyTotal());
+    }
+}
