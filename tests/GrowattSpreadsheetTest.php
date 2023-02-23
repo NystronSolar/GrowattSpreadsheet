bca7697423095e08f00522358464d84eac1c5613
TheDevick <erick@bilhalba.com.br>
2023-02-23 06:59:52 +0000

    test: create Growatt Spreadsheet Test

---
+++ tests/GrowattSpreadsheetTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use NystronSolar\GrowattSpreadsheet\Company\Company;
+use NystronSolar\GrowattSpreadsheet\GrowattSpreadsheet;
+use PHPUnit\Framework\TestCase;
+
+class GrowattSpreadsheetTest extends TestCase
+{
+    public function testGrowattSpreadsheetGetterSetter()
+    {
+        $fakeCompany = new Company();
+        $fakeCompany
+            ->setName("Fake Company")
+            ->setCode("FAKE1")
+            ->setTotalComponentPower(10000)
+            ->setEnergyTotal(5000.0)
+        ;
+
+        $growattSpreadsheet = new GrowattSpreadsheet();
+        $growattSpreadsheet
+            ->setCompany($fakeCompany)
+        ;
+
+        $this->assertSame($fakeCompany, $growattSpreadsheet->getCompany());
+    }
+}
